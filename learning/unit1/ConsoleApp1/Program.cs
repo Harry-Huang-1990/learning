df5de1bb659e6187fb74803fd5c08f5c04998dad
Harry-Huang <253871464@qq.com>
2026-03-27 14:54:55 +0800

    学习第一单元

---
+++ learning/unit1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //for (int i = 1; i <= 9; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write($"{j}*{i}=" + i * j + "    ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
+
+
+class Person
+{
+    public int Age;
+}
+
---
+++ learning/unit1/unit1.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using MR.Data;
+
+namespace MR.View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Model model_1 = new Model();
+            model_1.GetData();
+        }
+    }
+}
+
+namespace MR.Data
+    {
+    class Model
+    {
+        public void GetData()
+        {
+            Console.WriteLine("我爱你");
+            Console.ReadLine();
+            
+        }
+    }
+}
